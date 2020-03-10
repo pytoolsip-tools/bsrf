49b9516a8c9b04fc94fbbd159897e643eae0f997
JDreamHeart <15602291936@163.com>
2020-03-10 22:03:07 +0800

    refactor:更新工具模板

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off && setlocal enabledelayedexpansion
-
-cd run
-
-set pyexe=E:\project\ptip\project\PyToolsIP\include\python\python.exe
-set mainfile=main.py
-set buildfile=build.py
-
-run.vbs %pyexe% %mainfile% %buildfile%
